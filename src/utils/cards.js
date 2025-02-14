280189d2acb3646a9c44e35a432dd79fbe24f7c7
Polina Lepekhina <P0lliSun@yandex.ru>
2025-02-14 17:31:32 +0300

    Initial commit

---
+++ src/utils/cards.js
@@ -0,0 +1,32 @@
+const initialCards = [
+  {
+    image: "../assets/images/milk.png",
+    name: "Молоко 3,2 %",
+    about: "от Вадима Рошки",
+    price: "95 ₽",
+    piece: "0,93 л",
+  },
+  {
+    image: "../assets/images/bread.png",
+    name: "Хлеб пшеничный",
+    about: "от Дарьи и Марии",
+    price: "95 ₽",
+    piece: "930 г",
+  },
+  {
+    image: "../assets/images/eggs.png",
+    name: "Яйца куриные",
+    about: "от Евгения Рошаль",
+    price: "120 ₽",
+    piece: "10 шт",
+  },
+  {
+    image: "../assets/images/butter.png",
+    name: "Масло сливочное 82 %",
+    about: "от Вадима Рошки",
+    price: "290 ₽",
+    piece: "250 г",
+  },
+];
+
+export { initialCards };
